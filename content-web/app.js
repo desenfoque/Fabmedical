0378c1309b5c18013e134bd6b09136d1304dcdbc
desenfoque <csantis@gmail.com>
2022-02-24 19:26:27 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("aaca242e-8bd5-4712-8c17-3439d7007df6");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
